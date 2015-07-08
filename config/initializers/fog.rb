8349fa211a6cd50575b9d4f503e7f2898b2dcbb1
Mohamed Rachidi <rachidi29@gmail.com>
2015-07-08 10:04:51 -0700

    fix for analtyics display

---
+++ config/initializers/fog.rb
@@ -0,0 +1,5 @@
+FOG = Fog::Storage.new({
+                         :provider              => 'AWS',
+                         :aws_access_key_id     => 'AKIAJ2Y63M3LUOM3YQBQ',
+                         :aws_secret_access_key => '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'
+})
