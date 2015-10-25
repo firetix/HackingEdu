aa54507dcb0822a39edec03f533e255755abe85a
Mohamed Rachidi <rachidi29@gmail.com>
2015-10-24 23:19:51 -0400

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,8 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.for(:sign_up) << :full_name
     devise_parameter_sanitizer.for(:sign_up) << :wish_list
     devise_parameter_sanitizer.for(:sign_up) << :brands
+    devise_parameter_sanitizer.for(:sign_up) << :is_student
+    devise_parameter_sanitizer.for(:sign_in) << :is_student
     devise_parameter_sanitizer.for(:account_update) << :experience_level
     devise_parameter_sanitizer.for(:account_update) << :gender
     devise_parameter_sanitizer.for(:account_update) << :weight
@@ -23,6 +25,7 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.for(:account_update) << {brands:[]}
     devise_parameter_sanitizer.for(:account_update) << :brands
     devise_parameter_sanitizer.for(:account_update) << :photo_url
+    devise_parameter_sanitizer.for(:account_update) << :is_student
 
     # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:experience_level, :gender, :weight, :height,:birthdate)}
 
